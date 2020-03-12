4a06d3ad19f16f9afaf87f46effeb4501896c5a7
Alexandr <6alex6kash6@gmail.com>
2020-03-12 22:48:39 +0300

    feat:new functionality

--- script.js
+++ script.js
@@ -1,9 +1,64 @@
 const navigation = document.querySelector(".navigation");
+const submitButton = document.querySelector(".submit-button");
+const messageBox = document.getElementById("message-box");
+const okButton = document.getElementById("ok-button");
+const verticalPhone = document.getElementById("phone-vertical");
+const horizontalPhone = document.getElementById("phone-horizontal");
+const imageWrapper = document.querySelector(".portfolio__gallery");
 
 navigation.addEventListener("click", event => {
   navigation.querySelectorAll("a").forEach(element => {
     element.classList.remove("active");
   });
-
   event.target.classList.add("active");
+});
+
+verticalPhone.addEventListener("click", event => {
+  const verticalDisplay = document.getElementById("vertical-off");
+  if (verticalDisplay.classList.contains("vertical-off")) {
+    verticalDisplay.classList.remove("vertical-off");
+  } else {
+    verticalDisplay.classList.add("vertical-off");
+  }
+});
+
+horizontalPhone.addEventListener("click", event => {
+  const horizontalDisplay = document.getElementById("horizontal-off");
+  if (horizontalDisplay.classList.contains("horizontal-off")) {
+    horizontalDisplay.classList.remove("horizontal-off");
+  } else {
+    horizontalDisplay.classList.add("horizontal-off");
+  }
+});
+
+imageWrapper.addEventListener("click", event => {
+  if (event.target.tagName === "IMG") {
+    imageWrapper.querySelectorAll("img").forEach(item => {
+      item.classList.remove("img-active");
+    });
+    event.target.classList.add("img-active");
+  }
+});
+
+submitButton.addEventListener("click", () => {
+  const subject = document.getElementById("subject").value;
+  const textarea = document.getElementById("textarea").value;
+  const inputName = document.getElementById("name");
+  const inputEmail = document.getElementById("email");
+  document.getElementById("subject-input").innerText = subject;
+  document.getElementById("textarea-input").innerText = textarea;
+  if (subject === "") {
+    document.getElementById("subj-p").innerText = "Без темы";
+  }
+  if (subject === "") {
+    document.getElementById("describe-p").innerText = "Без описания";
+  }
+  if (inputName.checkValidity() && inputEmail.checkValidity()) {
+    event.preventDefault();
+  }
+  messageBox.classList.remove("hidden");
+});
+
+okButton.addEventListener("click", () => {
+  messageBox.classList.add("hidden");
 });
